34a2ca99ca53a8dc2ed2a8df37b9302505f7f04a
NastyaArt <artemchuk.nastya@gmail.com>
2017-06-15 18:01:32 +0300

    third commit

--- drawwindow.cpp
+++ drawwindow.cpp
@@ -8,27 +8,84 @@ DrawWindow::DrawWindow(QWidget *parent) : QWidget(parent)
 
 void DrawWindow::paintEvent(QPaintEvent *) {
 
-    QPainter p(this); // Создаём новый объект рисовальщика
-    p.setPen(QPen(Qt::red, 1, Qt::SolidLine)); // Настройки рисования
+    QPainter p(this);
 
     if (coordinates.GetPointsList().length()>0)
     {
-        setFixedSize((abs(coordinates.GetMinX())+abs(coordinates.GetMaxX()))*onePixel, (abs(coordinates.GetMinY())+abs(coordinates.GetMaxY()))*onePixel);
-        p.setWindow(QRect(coordinates.GetMinX()*onePixel, coordinates.GetMinY()*onePixel, (abs(coordinates.GetMinX())+abs(coordinates.GetMaxX()))*onePixel, (abs(coordinates.GetMinY())+abs(coordinates.GetMaxY()))*onePixel));
+        p.scale(0.8,0.8);
+        if (coordinates.GetMinX() >= 0) OxMin = -6*traitsDist;
+        else OxMin = coordinates.GetMinX()*onePixel-traitsDist;
+        if (coordinates.GetMinY() >= 0) OyMin = -6*traitsDist;
+        else OyMin = ((int)(coordinates.GetMinY()-0.5))*onePixel-traitsDist;
+        if (coordinates.GetMaxX() <= 0) OxMax = 6*traitsDist;
+        else OxMax = coordinates.GetMaxX()*onePixel+traitsDist;
+        if (coordinates.GetMaxY() <= 0) OyMax = 6*traitsDist;
+        else OyMax = ((int)(coordinates.GetMaxY()+0.5))*onePixel+traitsDist;
+        OxLength = (abs(OxMin)+abs(OxMax));
+        OyLength = (abs(OyMin)+abs(OyMax));
+
+        p.setWindow(QRect(OxMin, -OyMax, OxLength, OyLength));
+        setFixedSize(OxLength, OyLength);
+
+        p.setPen(QPen(Qt::lightGray, 1, Qt::SolidLine));
+        for (int i=OxMin; i<=OxMax; i+=traitsDist)
+        {
+            p.drawLine(i, -OyMax, i, -OyMin);
+        }
+        for (int i=-OyMax; i<=-OyMin; i+=traitsDist)
+        {
+            p.drawLine(OxMin, i, OxMax, i);
+        }
+
+        p.setPen(QPen(Qt::black, 1, Qt::SolidLine));
+
+        p.drawLine(OxMin, 0, OxMax, 0);
+        p.drawLine(0, -OyMin, 0, -OyMax);
+
+        p.drawLine(OxMax, 0, OxMax-10, numShift*2);
+        p.drawLine(OxMax, 0, OxMax-10, -numShift*2);
+        p.drawLine(0, -OyMax, numShift*2, -OyMax+10);
+        p.drawLine(0, -OyMax, -numShift*2, -OyMax+10);
+
+        p.setPen(QPen(Qt::green, 2, Qt::SolidLine));
         for (int i=0; i<coordinates.GetPointsList().length()-1; i++)
         {
-           // p.drawPoint(coordinates.GetPointsList().at(i).GetX(),coordinates.GetPointsList().at(i).GetY());
-             p.drawLine(coordinates.GetPointsList().at(i).GetX()*onePixel,coordinates.GetPointsList().at(i).GetY()*onePixel,coordinates.GetPointsList().at(i+1).GetX()*onePixel,coordinates.GetPointsList().at(i+1).GetY()*onePixel);
+            p.drawLine(coordinates.GetPointsList().at(i).GetX()*onePixel,-coordinates.GetPointsList().at(i).GetY()*onePixel,coordinates.GetPointsList().at(i+1).GetX()*onePixel,-coordinates.GetPointsList().at(i+1).GetY()*onePixel);
         }
-        p.setPen(QPen(Qt::black, 3, Qt::SolidLine));
+        p.setPen(QPen(Qt::darkGreen, 3, Qt::SolidLine));
         for (int i=0; i<coordinates.GetPointsList().length(); i++)
         {
-            p.drawPoint(coordinates.GetPointsList().at(i).GetX()*onePixel,coordinates.GetPointsList().at(i).GetY()*onePixel);
+            p.drawPoint(coordinates.GetPointsList().at(i).GetX()*onePixel,-coordinates.GetPointsList().at(i).GetY()*onePixel);
         }
-       // p.drawPoint(0,0);
         p.setPen(QPen(Qt::black, 1, Qt::SolidLine));
-        p.drawLine(-p.window().width()/2, 0, p.window().width()/2, 0);
-        p.drawLine(0, -p.window().height()/2, 0, p.window().height()/2);
+        for (int i=OxMin+traitsDist; i<OxMax; i+=5*traitsDist)
+        {
+            p.drawLine(i, numShift, i, -numShift);
+        }
+        for (int i=-OyMax+traitsDist; i<-OyMin; i+=5*traitsDist)
+        {
+            p.drawLine(numShift, i, -numShift, i);
+        }
+        float numXMin, numXMax;
+        if (coordinates.GetMinX() >= 0) numXMin = -1.0;
+        else  numXMin =coordinates.GetMinX();
+        if (coordinates.GetMaxX() <= 0) numXMax = 1.0;
+        else  numXMax =coordinates.GetMaxX();
+        for (float num=numXMin; num<=numXMax; num+=h)
+        {
+            p.drawText(num*onePixel-2*numShift, numShift, 2*numShift, 2*numShift, Qt::TextDontClip, QString::number(num));
+        }
+        float numYMin, numYMax;
+        if (coordinates.GetMinY() >= 0) numYMin = -1.0;
+        else  numYMin =((int)(coordinates.GetMinY()-0.5));
+        if (coordinates.GetMaxY() <= 0) numYMax = 1.0;
+        else  numYMax =((int)(coordinates.GetMaxY()+0.5));
+        for (float num=numYMin; num<=numYMax; num+=h)
+        {
+           if (num!=0.0f)
+           p.drawText(2*numShift, -num*onePixel-2*numShift, 2*numShift, 2*numShift, Qt::TextDontClip, QString::number(num));
+        }
+       //сделать координатную сетку
 
     }
 
@@ -39,3 +96,4 @@ void DrawWindow::SetCoordinates(CoordinatesList list)
     coordinates = list;
     repaint();
 }
+
--- drawwindow.h
+++ drawwindow.h
@@ -17,6 +17,15 @@ public:
 private:
     CoordinatesList coordinates;
     const int onePixel=100;
+    const int numShift=3;
+    const int traitsDist=10;
+    int OxLength;
+    int OyLength;
+    int OxMin;
+    int OyMin;
+    int OxMax;
+    int OyMax;
+    const float h=0.5f;
 protected:
     void paintEvent(QPaintEvent *);
 
